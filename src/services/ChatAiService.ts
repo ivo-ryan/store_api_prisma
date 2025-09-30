c57f0463c991ef8c47cc3972fa2e7cf3d2190599
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-09-29 21:12:11 -0300

    Update

--- src/services/ChatAiService.ts
+++ src/services/ChatAiService.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 export class ChatAiService {
     async chatResponse(message: string) {
         const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-        const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
+        const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
         const result = await model.generateContent(message);
        return result.response.text();
     }
