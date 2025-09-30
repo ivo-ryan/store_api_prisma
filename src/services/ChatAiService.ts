d1a7753cce13dbd460dc932f784519fc331dd41e
ivo-ryan <ivoryanalfredobastos@gmail.com>
2025-09-29 21:09:21 -0300

    Update Api_key

--- src/services/ChatAiService.ts
+++ src/services/ChatAiService.ts
@@ -2,7 +2,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai"
 
 export class ChatAiService {
     async chatResponse(message: string) {
-        const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY || "AIzaSyCHVsDdSiFDSBADvbMylWGjJdzOfw0FLbk");
+        const genAi = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
         const model = genAi.getGenerativeModel({ model: "gemini-1.5-flash" });
         const result = await model.generateContent(message);
        return result.response.text();
